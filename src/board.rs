588b1e317de827fba3e5e473f6dc95e10b20e4b2
Jamie Ly <jamie.ly@gmail.com>
2016-06-23 21:21:58 -0400

    Added indices

--- src/board.rs
+++ src/board.rs
@@ -1,6 +1,6 @@
 pub struct Board {
-    rows: i32,
-    columns: i32
+    rows: usize,
+    columns: usize
 }
 
 impl Board {
@@ -9,6 +9,11 @@ impl Board {
             rows: 6,
             columns: 7
         }
+    }
+    pub fn indices(&self) -> Vec<(usize, usize)> {
+        (0..self.columns).flat_map(|column: usize| {
+            (0..self.rows).map(move |row: usize| (column, row))
+        }).collect::<Vec<(usize, usize)>>()
     }
 }
 
@@ -21,6 +26,13 @@ mod test {
         let board = Board::new();
         assert!(board.rows == 6);
         assert!(board.columns == 7)
+    }
+
+    #[test]
+    fn it_should_have_rows_times_columns_indicies() {
+        let board = Board::new();
+        let indices = board.indices();
+        assert!(indices.len() == board.rows * board.columns);
     }
 }
 
