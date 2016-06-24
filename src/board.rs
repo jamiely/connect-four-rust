03f176c747bbad7d4c02f80cd81244dbd580e236
Jamie Ly <jamie.ly@gmail.com>
2016-06-24 11:10:08 -0400

    Implemented get/set marker

--- src/board.rs
+++ src/board.rs
@@ -1,10 +1,19 @@
 use std::collections::HashMap;
 
+type Index = (usize, usize);
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+enum Marker {
+    Empty,
+    X,
+    O,
+}
+
 pub struct Board {
     rows: usize,
     columns: usize,
-    indicies: Vec<(usize, usize)>,
-    markers: HashMap<(usize, usize), usize>
+    indicies: Vec<Index>,
+    markers: HashMap<Index, Marker>
 }
 
 impl Board {
@@ -14,11 +23,11 @@ impl Board {
 
         let indicies = (0..columns).flat_map(|column: usize| {
             (0..rows).map(move |row: usize| (column, row))
-        }).collect::<Vec<(usize, usize)>>();
+        }).collect::<Vec<Index>>();
 
         let mut markers = HashMap::new();
         for index in indicies.clone() {
-            markers.insert(index, 0 as usize);
+            markers.insert(index, Marker::Empty);
         }
 
         Board {
@@ -30,14 +39,22 @@ impl Board {
     }
 
     pub fn is_empty(&self) -> bool {
-        let zero: usize = 0;
-        self.markers.values().all(|v| zero.eq(v))
+        self.markers.values().all(|v| Marker::Empty.eq(v))
+    }
+
+    pub fn set_marker(&mut self, index: &Index, marker: Marker) -> () {
+        self.markers.insert(index.clone(), marker);
+    }
+
+    pub fn get_marker(&self, index: &Index) -> Option<Marker> {
+        self.markers.get(&index).map(|r| r.clone())
     }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use super::Marker;
 
     #[test]
     fn should_have_7_columns_and_6_rows() {
@@ -57,6 +74,17 @@ mod test {
     fn it_should_be_empty() {
         let board = Board::new();
         assert!(board.is_empty());
+    }
+
+    #[test]
+    fn it_should_keep_track_of_markers() {
+        let mut board = Board::new();
+        let index = (0, 0);
+        board.set_marker(&index, Marker::X);
+        match board.get_marker(&index) {
+            Some(marker) => assert_eq!(marker, Marker::X),
+            None         => panic!(),
+        }
     }
 }
 
